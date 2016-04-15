8ddcaf146a49d166a949ec0be42f0e183c992caf
Annie Cain <anniejocain@gmail.com>
2016-04-15 15:23:03 -0700

    owners cleanup

--- resources/views/owners/show.blade.php
+++ resources/views/owners/show.blade.php
@@ -5,14 +5,16 @@ View Project Owners
 @endsection
 
 @section('content')
+	<div class="row">
+		<div class="col-md-6">
 {!! Form::open(['url' => 'owners']) !!}
 @include('errors.list')
 <div class="form-group">
-	{!! Form::label('name', 'Owner Name: ') !!}
+	{!! Form::label('name', 'Owner Name ') !!}
 	{!! Form::text('name', null, ['class' => 'form-control']) !!}
 </div>
 <div class="form-group">
-	{!! Form::submit('Add Project Owner', ['class' => 'btn btn-primary form-control']) !!}
+	{!! Form::submit('Add Project Owner', ['class' => 'btn btn-primary']) !!}
 </div>
 {!! Form::close() !!}
 	<table class='table sortable-theme-bootstrap table-striped' data-sortable>
@@ -32,4 +34,6 @@ View Project Owners
 @endforeach
 		</tbody>
 	</table>
+</div>
+</div>
 @endsection
